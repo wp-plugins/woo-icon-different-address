bb9d36a65f4deb6129ec47d73f9b39fbec7e9463
woody062 <woody062@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 07:27:51 +0000

    First
    git-svn-id: https://plugins.svn.wordpress.org/woo-icon-different-address/trunk@1099001 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,33 @@
+=== Woo icon different address ===
+
+Contributors: Agence Best Of Site
+Tags: icon stock, woocommerce, products, themes, e-commerce, shop
+Requires at least: 3.8.1
+Tested up to: 4.1.1
+Tested with Woocommerce : 2.3.5
+Stable tag: 2.2.8
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Ajoute une icône dans le backoffice des commandes woocommerce.
+
+== Description ==
+
+Woo Icon different address, ajoute une icône dans le backoffice des commandes woocommerce. Voyez en un coup d'oeil si l'adresse de livraison est différente !
+Woo Icon different address, add an icon in the backoffice of WooCommerce orders. See with this icon if the delivery address is different !
+
+== Installation ==
+Installez le plugin et cliquez sur activer. 
+
+1. Dezipez le fichier
+2. Envoyez le fichier dans wp-content/plugins/
+3. Activez le plugin sur la page des extensions
+
+== Screenshots ==
+
+1. Adresse de livraison différente
+
+== Changelog ==
+
+= Version 1 =
+* Mise en ligne
---
+++ woo_icon_diff_address.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Woo icon different address
+Plugin URI: http://www.best-of-site.fr/plugin
+Description: Le pluginWoo icon different address affiche une icône visible pour avoir un apperçue rapide des adresses de livraison différentes.
+Version: 1
+Author: Best Of Site
+Author URI: http://best-of-site.fr
+*/
+
+/*  Copyright 2014  Agence Best Of Site  (email : contact@bestofsite.fr) */
+
+
+// ajout de la colone avec icône pour adresse différente
+add_action('woocommerce_checkout_update_order_meta', 'woo_icon_diff_address');
+function woo_icon_diff_address( $order_id ) {
+global $woocommerce;
+$diffaddress = $_POST['ship_to_different_address'];
+update_post_meta( $order_id, '_diff_address', $_POST['ship_to_different_address'] );
+}
+
+add_filter( 'manage_edit-shop_order_columns', 'woo_icon_column_address_diff', 20 );
+function woo_icon_column_address_diff( $columns ) {
+$offset = 9;
+$updated_columns = array_slice( $columns, 0, $offset, true) +
+array( 'shipping_adress_differente' => esc_html__( 'Adresse différente', 'woocommerce' ) ) +
+array_slice($columns, $offset, NULL, true);
+return $updated_columns;
+}
+// Populate weight column
+add_action( 'manage_shop_order_posts_custom_column', 'woo_custom_column_adress_diff', 2 );
+function woo_custom_column_adress_diff( $column ) {
+global $post, $woocommerce, $the_order;
+if ( $column == 'shipping_adress_differente' ) {
+$diff_address = get_post_meta( $post->ID, '_diff_address', true );
+ if(!empty ($diff_address)) 
+echo '<span data-tip="'.esc_html__( 'Attention à l\'adresse de livraison', 'woocommerce' ).'" style="color:#E9B10B;font-size:28px;" class="dashicons tips dashicons-info"></span>';
+else echo'';
+}
+}
